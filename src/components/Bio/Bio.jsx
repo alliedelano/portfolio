17bc7ed60dc002e9d2c683730cbf39d0760d7487
Allie Delano <alliedelano@Allies-MacBook-Pro.local>
2021-08-12 15:42:25 -0700

    adds bio and photo to about me page

--- src/components/Bio/Bio.jsx
+++ src/components/Bio/Bio.jsx
@@ -7,13 +7,38 @@ export default function Bio(){
     return(
         <>
             <div className="bio-profile">
+                <br />
+                <Segment className="bio-segment" textAlign="left">
+                    <Image src={picture} size="small" floated="right" inline spaced/>
+                    <h3>
+                    Full Stack Developer  |  Educator  |  Adventurer
+                    </h3>
 
-            <Segment className="bio-segment">
-                <Image src={picture} size="small" floated="left" inline spaced/>
-                <p>Some text</p>
-                <p>Other text</p>
-            </Segment>
-
+                    <p>
+                        Friends and colleagues (and even my own mom) call me “Adventure Allie.”  
+                        I run in the mountains, jump from planes, and love experiencing the world 
+                        around me and learning as much as I can at every opportunity.  Originally from Virginia, I’ve lived in San Diego since early 2020 and it’s 
+                        quickly become home.
+                    </p>
+                    <p>
+                        Although I’ve never worked in tech, I’ve always looked for ways to make life easier 
+                        through creating systems and adapting technology. Over the last few years, I’ve 
+                        designed and implemented systems for human resource allocation and forecast, partnerships, 
+                        project management, training new team members, cataloguing tools and resources, and data 
+                        collection. More recently, as I tried to update these systems to make them more efficient 
+                        and collaborative, I found my tech skills kept hitting a wall. Unsatisfied with 
+                        accepting this wall, I enrolled in a full-time Software Engineering Immersive program and 
+                        added full-stack development skills to my toolkit.
+                    </p>
+                    <p>
+                        I’m excited to keep creating, building, and innovating and look forward to working with 
+                        organizations I’ve known for years, as well as those I have yet to meet. My goal is to 
+                        continue helping tiny teams with big dreams to scale their impact with technology and 
+                        systems that truly fit their needs. If you’d like to build something together, please 
+                        get in touch!
+                    </p>
+                    <br />
+                </Segment>
             </div>
 
         </>
--- src/components/Tagline/Tagline.jsx
+++ src/components/Tagline/Tagline.jsx
@@ -8,7 +8,9 @@ export default function Tagline(){
         <>  
             <div className="home-tagline">
                 <Container stackable fluid text className="about-me-container" >
-                    <h2 className="tagline">A systems thinker who recently added full-stack web development skills to her toolkit and is helping tiny teams with big dreams scale their impact.</h2>
+                    <h2 className="tagline">I'm fueled by curiosity and optimism and tend to jump into everything I do with both feet and my entire heart.
+                    I listen, synthesize, create, and advocate, and am a tenacious problem-solver, athlete, and adventurer.
+                    </h2>
                     <br />
                     <Link to="/aboutme"><Button color="blue" className="btn">About</Button></Link>
                     <Link to="/projects"><Button color="blue" className="btn">Projects</Button></Link>
